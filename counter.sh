bd17096e23729ada2b783239d52a100d49be669b
Alex <realtowz@yahoo.com>
2020-12-12 19:19:28 +0200

    Show totals

--- counter.sh
+++ counter.sh
@@ -3,7 +3,7 @@
 
 if [ $# -ne 1 ]
 then
-    echo"give directory path"
+    echo "give directory path"
      exit 1
 fi
 
@@ -22,5 +22,10 @@ fi
 
 
 varc=$(wc -m $1/* | awk '{print $1}')
-
 varl=$(wc -l $1/* | awk '{print $1}')
+varavg=$((varc/varl))
+
+
+echo "Total characters: $varc"
+echo "Total lines: $varl"
+echo "Average: $varavg"
